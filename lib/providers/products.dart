491e524a248ca58cfbdb8a7f33e9ae6aa5534635
Javi <javivillarreal@23branches.com>
2022-08-09 12:39:09 -0500

    connected database

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import './product.dart';
 
@@ -64,6 +67,19 @@ class Products with ChangeNotifier {
   // }
 
   void addProduct(Product product) {
+    const url =
+        'https://flutter-shop-app-9fa69-default-rtdb.firebaseio.com/products.json';
+    http.post(
+      Uri.parse(url),
+      body: json.encode({
+        'title': product.title,
+        'description': product.description,
+        'imageUrl': product.imageUrl,
+        'price': product.price,
+        'isFavorite': product.isFavorite,
+      }),
+    );
+
     final newProduct = Product(
       title: product.title,
       description: product.description,
