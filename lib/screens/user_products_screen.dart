09266884a23eb333a795e2be1516f3053ff33fad
Javi <javivillarreal@23branches.com>
2022-08-09 11:59:03 -0500

    fixed bugs

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -28,16 +28,18 @@ class UserProductsScreen extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.all(8),
         child: ListView.builder(
-            itemCount: productsData.items.length,
-            itemBuilder: (_, i) => Column(
-                  children: [
-                    UserProductItem(
-                      productsData.items[i].title,
-                      productsData.items[i].imageUrl,
-                    ),
-                    Divider(),
-                  ],
-                )),
+          itemCount: productsData.items.length,
+          itemBuilder: (_, i) => Column(
+            children: [
+              UserProductItem(
+                productsData.items[i].id,
+                productsData.items[i].title,
+                productsData.items[i].imageUrl,
+              ),
+              Divider(),
+            ],
+          ),
+        ),
       ),
     );
   }
